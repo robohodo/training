1c27759489e8084c9af7ca86f3b2a67a2c250f1c
robohodo <robo.hodo@gmail.com>
2016-12-13 21:56:54 -0600

    updating

--- VAGRANT/bento/centos-7.2_puppet2/bootstrap.sh
+++ VAGRANT/bento/centos-7.2_puppet2/bootstrap.sh
@@ -4,6 +4,13 @@
 
 touch /.tomcat.server
 
+# Update /etc/hosts 
+cat >> /etc/hosts << EOF 
+192.168.140.128 robows-puppet.localdomain robows-puppet #00 0c 29 b5 b5 86 
+192.168.140.129 robows-jenkins.localdomain robows-jenkins #00 0c 29 2d 43 fb
+192.168.0.3 robows.localdomain robows #90 b1 1c 7a a7 c7
+EOF
+
 # Register with puppet 
 curl -k https://robows-puppet.localdomain:8140/packages/current/install.bash | bash
 
